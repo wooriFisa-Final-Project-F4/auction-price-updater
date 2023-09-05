bfb831f525b910b6beb32338593780692e847ecc
endlessmomo <soohyuk96@naver.com>
2023-09-05 18:31:38 +0900

    [#3][refactoring] 더티 체킹 Native Query로 전환

--- src/main/java/f4/domain/persist/repository/ProductRepository.java
+++ src/main/java/f4/domain/persist/repository/ProductRepository.java
@@ -2,9 +2,12 @@ package f4.domain.persist.repository;
 
 import f4.domain.persist.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product,Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
+  @Query(value = "UPDATE product SET bid_user_id = :bidUserId, auction_price = :auctionPrice WHERE id =: productId",nativeQuery = true)
+  void updateBidUserIdAndPriceById(Long productId, Long bidUserId, String auctionPrice);
 }
