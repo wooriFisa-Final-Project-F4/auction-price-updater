ab73430a5324ffc8eee0d9270ff0fabe2dec92c7
endlessmomo <soohyuk96@naver.com>
2023-09-05 09:15:20 +0900

    [#3][refactoring] kafka 관련 패키지 어노테이션 순서 정리

--- src/main/java/f4/domain/kafka/Consumer.java
+++ src/main/java/f4/domain/kafka/Consumer.java
@@ -1,22 +1,22 @@
 package f4.domain.kafka;
 
-import f4.domain.dto.request.KafkaDTO;
-import f4.domain.service.impl.PriceServiceImpl;
+import f4.domain.dto.request.KafkaDto;
+import f4.domain.service.impl.AuctionPriceServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-@Service
 @Slf4j
+@Service
 @RequiredArgsConstructor
 public class Consumer {
 
-  private final PriceServiceImpl priceService;
+  private final AuctionPriceServiceImpl priceService;
 
   @KafkaListener(topics = "${kafka.topic.name}")
-  public void consume(ConsumerRecord<String, KafkaDTO> record) {
+  public void consume(ConsumerRecord<String, KafkaDto> record) {
     try {
       priceService.checkAndUpdate(record.value());
     } catch (Exception e) {
--- src/main/java/f4/domain/kafka/Producer.java
+++ src/main/java/f4/domain/kafka/Producer.java
@@ -1,6 +1,6 @@
 package f4.domain.kafka;
 
-import f4.domain.dto.response.ProductDTO;
+import f4.domain.dto.response.ProductDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -11,13 +11,13 @@ public class Producer {
   @Value(value = "${kafka.produce.topic.name}")
   private String topicName;
 
-  private KafkaTemplate<String, ProductDTO> kafkaTemplate;
+  private KafkaTemplate<String, ProductDto> kafkaTemplate;
 
-  public Producer(KafkaTemplate<String, ProductDTO> kafkaTemplate) {
+  public Producer(KafkaTemplate<String, ProductDto> kafkaTemplate) {
     this.kafkaTemplate = kafkaTemplate;
   }
 
-  public void produce(ProductDTO data){
-    kafkaTemplate.send(topicName,data.getProductName(),data);
+  public void produce(ProductDto data) {
+    kafkaTemplate.send(topicName, data.getProductName(), data);
   }
 }
